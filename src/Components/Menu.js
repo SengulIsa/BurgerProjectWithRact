ea9cd5a43f1e430bf14a118b6e744f13f8835967
İsa ŞENGÜL <117285107+SengulIsa@users.noreply.github.com>
2024-02-24 17:08:14 +0300

    Update Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Data} from '../Helpers/Data';
+import  MenuItem  from '../Components/MenuItem';
+import '../Styles/Menu.css';
 
 export const Menu = () => {
   return (
-    <div>Menu</div>
-  )
+    <div className='menu'>
+      <h1 className='menuTitle'>Our Burgers</h1>
+      <div className='menuList'>
+        {Data.map((menuItem,key)=>{
+          return(
+            <MenuItem
+              key ={key}
+              image ={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+            />
+          );
+        })}
+
+      </div>
+    </div>
+  );
 }
