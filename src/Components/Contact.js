88bc6f38da0dc87487e2e3bf24550793fb4eaea5
İsa ŞENGÜL <117285107+SengulIsa@users.noreply.github.com>
2024-02-24 16:56:29 +0300

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import banner from '../assets/banner.png';
+import '../Styles/Contact.css';
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='leftSide' style={{backgroundImage:`url(${banner})`}}>
+      </div>
+      <div className='rightSide'>
+        <h1>Contact US</h1>
+        <form>
+          <label>Name-Surname</label>
+          <input type="text"  name='name' placeholder='Please Enter your Name and Surname...'/>
+          <label>Email</label>
+          <input type="email"  name='email' placeholder='Please Enter Your Mail'/>
+          <label>Your Message</label>
+          <textarea rows="6" name='message' placeholder='Enter Your Message'></textarea>
+
+        </form>
+      </div>
+    </div>
   )
 }
+
