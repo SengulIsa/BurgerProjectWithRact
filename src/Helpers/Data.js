6129e8d90cf1548ad5218e5275fd87f38387b968
İsa ŞENGÜL <117285107+SengulIsa@users.noreply.github.com>
2024-02-24 17:11:39 +0300

    Add Data.js file into Helper Folder For Menu item

---
+++ src/Helpers/Data.js
@@ -0,0 +1,77 @@
+import BurgerImage from '../assets/burger.jpg'
+
+export const Data =[
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+    {
+        name:'Efso Burger',
+        image: BurgerImage,
+        content: '150 Gr Efso Burger with potato and pickle',
+        price: 250,
+    },
+]
+
